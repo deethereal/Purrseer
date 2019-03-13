5fae8cc19e9175cde584ba8d53d4981fef47cfc1
Vladislav <freeman1813@gmail.com>
2019-03-13 23:03:29 +0300

    Закомментированный повар интроверт,держит все в себе, а не в базе данных

--- povar.js
+++ povar.js
@@ -11,12 +11,13 @@ var tasks=[],
 
 server.on('request', function (req, res){
 	if (req.method == 'GET'){
-	res.writeHead(200)
-	var newTask=tasks.shift();
-	var id = Date.now+randInt(1,1234567);
-	givenTasks[id]=newTask;
+	res.writeHead(200)//говорим, что все ок
+	var newTask=tasks.shift(); //Берем новую ссылку из массива заданий
+	var id = Date.now+randInt(1,1234567); //Создаем заданию id
+	givenTasks[id]=newTask;//переносим заданиев массив с выданными заданиями
+
 	res.write(id);
-	res.write(newTask);
+	res.write(newTask);//отправляем id задания и ссылку, на которой надо искать
 	res.end;
 
 	} else {
@@ -29,24 +30,25 @@ server.on('request', function (req, res){
 
 			req.on('end', function(){
 				var post = qs.parse(body);
-				if (givenTasks[post.id]){
 
-					tasks=tasks.concat(post.postcards);
+				if (givenTasks[post.id]){ //проверяем, не левый ли это запрос
 
-					if (treasureBox[givenTasks[post.id]]){
-                    	
-					} else{ treasureBox[givenTasks[post.id]]=''};
+					tasks=tasks.concat(post.postcards); //добавляем найденный ссылки в массив с заданиями
 
+					if (treasureBox[givenTasks[post.id]]){} 
+						else{ treasureBox[givenTasks[post.id]]=''};
 					for (item in post.treasures){
 						treasureBox[givenTasks[post.id]]+=post.treasures[item]
-					};
+					}; //Кладем найденные по ссылке слова в объект с полем <ссылка>
+
+
 					givenTasks.delete(post.id);
 					res.writeHead(200);
 					res.write("OK");
-					res.end();
+					res.end(); //удаляем задание из ожидаемых и говорим, что все хорошо
 				} else{
 					tasks.push(givenTasks[post.id]);
-					givenTasks.delete(post.id);
+					givenTasks.delete(post.id); //Если запрос левый, то возвращаем задание из ожидаемых в массив заданий
 					res.writeHead(423);
 					res.write("MUDA");
 					res.end();
